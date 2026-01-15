0a29b11acba44a2172b74c4df89f75a609bfa957
Armandd1 <armandtotos50@gmail.com>
2026-01-15 12:13:36 +0200

    fastapi hello endpoint

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #First FastAPI project
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def root():
+    return {'message': 'Hello World'}
+
+@app.get('/hello2')
+def read_root():
+    return {'message': 'World, again!'}
